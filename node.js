8742915d9f74150d731053157537c04f8e521643
webdev-abhi <78739734+webdev-abhi@users.noreply.github.com>
2021-07-24 01:21:12 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,6 @@
+alert("CLICK THE CAKE 🎂 AND ENJOY THE POWRRRY😁😁😁 🎶🎉🎊");
+let cake= document.getElementsByClassName('cake');
+window.addEventListener('click', function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
